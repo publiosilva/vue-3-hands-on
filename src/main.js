214b9423ca6ce14e94c1467fe2b72c7e5b54eb35
Publio Blenilio <publio.blenilio@gmail.com>
2022-06-23 16:25:01 -0300

    add gtag

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VueGtag from "vue-gtag";
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +9,16 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(
+  VueGtag,
+  {
+    appName: "My application",
+    pageTrackerScreenviewEnabled: true,
+    config: {
+      id: "G-1ZGCVS46HC",
+    },
+  },
+  router
+);
 
 app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import TestView from "../views/TestView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,7 @@ const router = createRouter({
     {
       path: "/test",
       name: "test",
-      component: () => import("../views/TestView.vue"),
+      component: TestView,
     },
   ],
 });

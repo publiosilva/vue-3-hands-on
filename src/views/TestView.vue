8e4ae5e6089409ce43901e6722d8b3fb83d5e4ba
Publio Blenilio <publio.blenilio@gmail.com>
2022-06-27 12:25:41 -0300

    update docs

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="test">
     <h1>This is an test page</h1>
+    <div>
+      <button @click="track">Track</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+function track() {
+  this.$gtag.event("anything A", {
+    event_category: "anything B",
+    event_label: "anything C",
+    value: "anything D",
+  });
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
